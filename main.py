858fb34918653e3c44c25eced6a0fcbb3a5a0b75
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2021-12-19 15:08:45 +0000

    css

--- main.py
+++ main.py
@@ -9,5 +9,9 @@ app = Flask('app')
 def index():
   return render_template("index.html")
 
+@app.route("/signup")
+def signup():
+  return render_template("signup.html")
+
 #Running
 app.run(host='0.0.0.0', port=8080)
