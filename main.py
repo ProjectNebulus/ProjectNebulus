593fb751ef8865c660689d141dcd8faba5d20048
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2021-12-19 06:18:35 +0000

    added css

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from flask import Flask
+#Imports
+from flask import Flask, render_template
+
+#Variables
 app = Flask('app')
 
+#Routing
 @app.route('/')
 def index():
   return render_template("index.html")
 
+#Running
 app.run(host='0.0.0.0', port=8080)
