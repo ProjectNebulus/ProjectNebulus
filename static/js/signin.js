6639fe230f26d66e0703c853aab4329b1ebe234d
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2022-02-03 21:05:10 +0000

    css stuff

--- static/js/signin.js
+++ static/js/signin.js
@@ -6,7 +6,7 @@ window.addEventListener('load', function () {
     submit.disabled = true;
     submit.style.color = 'gray';
     submit.style.backgroundColor = '#006097';
-    function launch_SSO()(){
+    function launch_SSO(){
       document.getElementById("signin_form").style.display = "none";
     }
     function checkValidUsername() {
