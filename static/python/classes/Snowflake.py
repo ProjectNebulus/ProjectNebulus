a9068a8de9ae1e38d24ea96f2025d40323f894c1
CoderN-P <76001641+CoolCoderN@users.noreply.github.com>
2022-02-13 19:15:43 -0800

    fix error with generating random ids

--- static/python/classes/Snowflake.py
+++ static/python/classes/Snowflake.py
@@ -3,4 +3,4 @@ from datetime import datetime
 
 @dataclass
 class Snowflake:
-    _id: int = datetime.timedelta(datetime.now() - datetime(2021, 12, 1)).total_seconds() * 1000
+    _id: int = (datetime.now() - datetime(2021, 12, 1)).total_seconds() * 1000
