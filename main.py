a8ef051c91cb850a14d9819c547946a3b416e347
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2022-02-22 00:49:03 +0000

    stuff

--- main.py
+++ main.py
@@ -600,5 +600,5 @@ def logout_from_schoology():
 app.add_url_rule(
     "/graphql", view_func=GraphQLView.as_view("graphql", schema=schema.graphql_schema, graphiql=True)
 )
-#serve(app, host="0.0.0.0", port="8000")
-app.run(host="localhost", port=8080)
+serve(app, host="0.0.0.0", port="8000")
+#app.run(host="localhost", port=8080)
