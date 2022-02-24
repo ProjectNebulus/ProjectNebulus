ca48f2a2479e399f7ee1aecc2f4115450790c517
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2022-02-24 15:19:47 +0000

    changed more css

--- static/js/createCourse.js
+++ static/js/createCourse.js
@@ -147,7 +147,7 @@ const templates = [
 
 for (const template of templates) {
     let button = document.createElement('span');
-    button.className = 'button';
+    button.className = 'button text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2';
     button.onclick = function () {
         chooseTemplate(template);
     };
