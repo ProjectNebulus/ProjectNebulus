395252fbfbc61e33c03149c8acd9e23b91256285
Lint Action <lint-action@samuelmeuli.com>
2022-02-27 15:57:53 +0000

    Fix code style issues with Black

--- main.py
+++ main.py
@@ -502,13 +502,16 @@ def music():
         "music.html", page="Nebulus - Music", user=session.get("username")
     )
 
+
 from flask import make_response
 
-@app.route('/manifest')
+
+@app.route("/manifest")
 def manifest():
-    res = make_response(render_template('manifest.appcache'), 200)
+    res = make_response(render_template("manifest.appcache"), 200)
     res.headers["Content-Type"] = "text/cache-manifest"
     return res
+
 
 @app.route("/holidays")
 def vh():
