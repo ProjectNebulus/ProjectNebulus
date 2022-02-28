4c732fd1caddb7e9546a41a643f6db599f01a8c3
Vedh Reddy <vedhkreddy@gmail.com>
2022-02-28 12:10:32 -0800

    added a starting point for time management

---
+++ static/python/managemnetalgorithm.py
@@ -0,0 +1,18 @@
+"""
+Essentially this is a time management
+
+you input how many hours you have and how much time that you want
+
+--base-- makes intervals
+
+--premium
+"""
+
+def basefunction(timestudy, timerelax):
+    timeneeded = timestudy - timerelax
+    remainder = timeneeded % 25
+    if remainder < 10:
+        return int(timeneeded / timerelax)
+    else:
+        return int(timeneeded / timerelaxed) + 1
+
