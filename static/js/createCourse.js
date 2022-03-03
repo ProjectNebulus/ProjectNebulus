7d4369476b16e54bc2c8691ee2f110d5418015fa
NicholasWang <nicholas.x.wang@gmail.com>
2022-03-02 17:25:11 -0800

    fix: fixed identifer already assigned error

--- static/js/createCourse.js
+++ static/js/createCourse.js
@@ -1,15 +1,15 @@
 // set up templates
-let divs = document.getElementsByClassName('scroll');
-let screens = document.getElementsByClassName('CourseBtn');
+var divs = document.getElementsByClassName('scroll');
+var screens = document.getElementsByClassName('CourseBtn');
 
 // set up button
-let modal = document.getElementById('CourseModal');
-let btn = document.getElementById('create');
+var modal = document.getElementById('CourseModal');
+var btn = document.getElementById('create');
 
 btn.onclick = function () {
     modal.style.display = 'block';
 
-    for (let screen of screens) screen.style.display = 'none';
+    for (var screen of screens) screen.style.display = 'none';
     screens[0].style.display = 'block';
 };
 
@@ -20,7 +20,7 @@ window.onclick = function (event) {
 };
 
 // set up close button
-for (let close of document.getElementsByClassName('close')) {
+for (var close of document.getElementsByClassName('close')) {
     close.className += ' material-icons dark:text-white';
     close.innerHTML = 'close';
     close.onclick = function () {
@@ -29,13 +29,13 @@ for (let close of document.getElementsByClassName('close')) {
 }
 
 // set up user
-const user = document.getElementById('userinfo').innerHTML;
+var user = document.getElementById('userinfo').innerHTML;
 
 // set up course stuff
-let courseName = document.getElementById('course-name');
-let courseTeacher = document.getElementById('course-teacher');
+var courseName = document.getElementById('course-name');
+var courseTeacher = document.getElementById('course-teacher');
 
-for (const element of document.getElementsByClassName("CourseBtn")) {
+for (var element of document.getElementsByClassName("CourseBtn")) {
     element.className += " hidden overflow-visible fixed right-0 left-0 top-4 z-50 justify-center items-center h-modal md:h-full md:inset-0";
     element.oncontextmenu = () => rightClickElements = {};
 }
@@ -43,7 +43,7 @@ for (const element of document.getElementsByClassName("CourseBtn")) {
 function createCourse(subtemplate) {
     document.getElementById('create-course-status').innerHTML = 'Creating course...';
 
-    const xhttp = new XMLHttpRequest();
+    var xhttp = new XMLHttpRequest();
     xhttp.open('POST', '/createCourse', true);
     xhttp.setRequestHeader('Content-type', 'application/json');
     xhttp.send(
@@ -58,7 +58,7 @@ function createCourse(subtemplate) {
 function skipTemplates() {
     screens[0].style.display = "none";
     customize(null, user + "'s class");
-    const h1 = document.getElementById("change-if-skip-templates");
+    var h1 = document.getElementById("change-if-skip-templates");
     h1.innerHTML = h1.innerHTML.replace("Step 3: ", "");
 }
 
@@ -73,7 +73,7 @@ description (e.g. Physics, Chemistry, Biology)
 as demonstrated below.
 
 */
-const templates = [
+var templates = [
     {
         name: 'Science',
         icon: 'science.svg',
@@ -150,18 +150,18 @@ const templates = [
     }
 ];
 
-for (const template of templates) {
-    let button = document.createElement('span');
+for (var template of templates) {
+    var button = document.createElement('span');
     button.className = 'button text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2';
     button.onclick = function () {
         chooseTemplate(template);
     };
 
     // TODO: fix duplicate code fragments
-    let imageSpan = document.createElement('span');
+    var imageSpan = document.createElement('span');
     imageSpan.style.float = 'left';
 
-    let image = document.createElement('img');
+    var image = document.createElement('img');
     image.style.float = 'right';
     image.height = 20;
     image.width = 20;
@@ -170,11 +170,11 @@ for (const template of templates) {
 
     button.appendChild(imageSpan);
 
-    let templateName = document.createElement('span');
+    var templateName = document.createElement('span');
     templateName.innerHTML = template.name;
     button.appendChild(templateName);
 
-    let next = document.createElement('span');
+    var next = document.createElement('span');
     next = document.createElement('img');
     next.style.float = 'right';
     next.height = 20;
@@ -184,12 +184,12 @@ for (const template of templates) {
 
     button.appendChild(document.createElement('br'));
 
-    let description = document.createElement('span');
+    var description = document.createElement('span');
     description.classList.add('text-gray-600', 'dark:text-gray-300')
     description.style.fontSize = '0.8em';
     description.innerHTML = 'Subtemplates: ';
 
-    for (const subtemplate of template.subtemplates) description.innerHTML += subtemplate + ', ';
+    for (var subtemplate of template.subtemplates) description.innerHTML += subtemplate + ', ';
     description.innerHTML = description.innerHTML.substring(0, description.innerHTML.length - 2);
 
     button.appendChild(description);
@@ -202,18 +202,20 @@ function chooseTemplate(template) {
     screens[1].style.display = 'block';
 
     divs[1].innerHTML = '';
-    for (const subtemplate of template.subtemplates) {
-        let button = document.createElement('span');
+    alert(template.subtemplates);
+    console.log(template.subtemplates);
+    for (var subtemplate of template.subtemplates) {
+        var button = document.createElement('span');
         button.className = 'button text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2';
         button.onclick = function () {
-            const name = template.name;
+            var name = template.name;
             customize(name, subtemplate);
         };
 
-        let imageSpan = document.createElement('span');
+        var imageSpan = document.createElement('span');
         imageSpan.style.float = 'left';
 
-        let image = document.createElement('img');
+        var image = document.createElement('img');
         image.style.float = 'right';
         image.height = 20;
         image.width = 20;
@@ -222,11 +224,11 @@ function chooseTemplate(template) {
 
         button.appendChild(imageSpan);
 
-        let templateName = document.createElement('span');
+        var templateName = document.createElement('span');
         templateName.innerHTML = subtemplate;
         button.appendChild(templateName);
 
-        let next = document.createElement('span');
+        var next = document.createElement('span');
         next = document.createElement('img');
         next.style.float = 'right';
         next.height = 20;
@@ -256,8 +258,8 @@ function importSchoology() {
     screens[2].style.display = 'none';
     screens[3].style.display = 'block';
 
-    const status = document.getElementById('create-course-status');
-    const input = document.getElementById("schoology-id");
+    var status = document.getElementById('create-course-status');
+    var input = document.getElementById("schoology-id");
 
     document.getElementById('schoology_import').onsubmit = function () {
         var index = input.value.indexOf(".schoology.com/course/");
@@ -280,12 +282,12 @@ function importSchoology() {
             return;
         }
 
-        const id = input.value.substring(index, endIndex);
+        var id = input.value.substring(index, endIndex);
 
         status.innerHTML
         status.innerHTML = 'Creating course...';
 
-        const xhttp = new XMLHttpRequest();
+        var xhttp = new XMLHttpRequest();
         xhttp.open('POST', '/createCourseSchoology', true);
         xhttp.setRequestHeader('Content-type', 'application/json');
         xhttp.send(
