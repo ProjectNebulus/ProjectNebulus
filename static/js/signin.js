8e72c62ec05d6fa5e866ffbb2b3effe0c629ebca
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2022-03-07 14:53:40 +0000

    added more css to login

--- static/js/signin.js
+++ static/js/signin.js
@@ -14,7 +14,7 @@ window.addEventListener('load', function () {
     submit.style.backgroundColor = '#006097';
     function checkValidUsername() {
         const response = document.getElementsByClassName('response')[0];
-
+        response.classList.add("text-blue-600")
         response.innerHTML = loadingIcon + 'Please wait...';
 
         if (document.getElementById('usrname').value === '') {
@@ -55,7 +55,8 @@ window.addEventListener('load', function () {
 
     function checkValidPassword() {
         const response = document.getElementsByClassName('response')[1];
-        response.innerHTML = loadingIcon + ' Please wait...';
+        response.classList.add("text-blue-600")
+        response.innerHTML = loadingIcon + 'Please wait...';
 
         if (document.getElementById('psw').value === '') {
             response.innerHTML =
@@ -120,7 +121,25 @@ function reqListener1() {
     if (this.responseText === 'true') {
         response.style.color = 'green';
         response.innerHTML = '<p class="material-icons">check_circle</p>';
-
+        
+         document
+            .getElementById('usrname')
+            .classList.remove(
+              'bg-red-50',
+              'border',
+              'border-red-500',
+              'text-red-900',
+              'placeholder-red-700',
+              'text-sm',
+              'rounded-lg',
+              'focus:ring-red-500',
+              'focus:border-red-500',
+              'block',
+              'w-full',
+              'p-2.5',
+        'dark:bg-red-100',
+              'dark:border-red-400'
+            );
         document
             .getElementById('usrname')
             .classList.add(
@@ -139,28 +158,48 @@ function reqListener1() {
                 'dark:bg-green-100',
                 'dark:border-green-400'
             );
+     
         validUser = true;
     } else if (this.responseText === 'false') {
         response.style.color = 'red';
         response.innerHTML = '<p class="material-icons">error</p>';
-        document
+      document
             .getElementById('usrname')
             .classList.add(
-                'bg-red-50',
+                'g-green-50',
                 'border',
-                'border-red-500',
-                'text-red-900',
-                'placeholder-red-700',
+                'border-green-500',
+                'text-green-900',
+                'placeholder-green-700',
                 'text-sm',
                 'rounded-lg',
-                'focus:ring-red-500',
-                'focus:border-red-500',
+                'focus:ring-green-500',
+                'focus:border-green-500',
                 'block',
                 'w-full',
                 'p-2.5',
-                'dark:bg-red-100',
-                'dark:border-red-400'
+                'dark:bg-green-100',
+                'dark:border-green-400'
             );
+        document
+            .getElementById('usrname')
+            .classList.add(
+              'bg-red-50',
+              'border',
+              'border-red-500',
+              'text-red-900',
+              'placeholder-red-700',
+              'text-sm',
+              'rounded-lg',
+              'focus:ring-red-500',
+              'focus:border-red-500',
+              'block',
+              'w-full',
+              'p-2.5',
+        'dark:bg-red-100',
+              'dark:border-red-400'
+            );
+        
         validUser = false;
     }
 }
@@ -172,6 +211,24 @@ function reqListener2() {
     if (this.responseText === 'true') {
         response.style.color = 'green';
         response.innerHTML = '<p class="material-icons">check_circle</p>';
+        document
+            .getElementById('psw')
+            .classList.remove(
+              'bg-red-50',
+              'border',
+              'border-red-500',
+              'text-red-900',
+              'placeholder-red-700',
+              'text-sm',
+              'rounded-lg',
+              'focus:ring-red-500',
+              'focus:border-red-500',
+              'block',
+              'w-full',
+              'p-2.5',
+        'dark:bg-red-100',
+              'dark:border-red-400'
+            );
         document
             .getElementById('psw')
             .classList.add(
@@ -194,24 +251,43 @@ function reqListener2() {
     } else if (this.responseText === 'false') {
         response.style.color = 'red';
         response.innerHTML = '<p class="material-icons">error</p>';
-        document
+      document
             .getElementById('psw')
-            .classList.add(
-                'bg-red-50',
+            .classList.remove(
+                'g-green-50',
                 'border',
-                'border-red-500',
-                'text-red-900',
-                'placeholder-red-700',
+                'border-green-500',
+                'text-green-900',
+                'placeholder-green-700',
                 'text-sm',
                 'rounded-lg',
-                'focus:ring-red-500',
-                'focus:border-red-500',
+                'focus:ring-green-500',
+                'focus:border-green-500',
                 'block',
                 'w-full',
                 'p-2.5',
-                'dark:bg-red-100',
-                'dark:border-red-400'
+                'dark:bg-green-100',
+                'dark:border-green-400'
             );
+        document
+            .getElementById('psw')
+            .classList.add(
+              'bg-red-50',
+              'border',
+              'border-red-500',
+              'text-red-900',
+              'placeholder-red-700',
+              'text-sm',
+              'rounded-lg',
+              'focus:ring-red-500',
+              'focus:border-red-500',
+              'block',
+              'w-full',
+              'p-2.5',
+        'dark:bg-red-100',
+              'dark:border-red-400'
+            );
+        
         validPass = false;
     }
 }
@@ -236,6 +312,7 @@ function enableButton() {
 setInterval(enableButton, 200);
 
 function loginUser() {
+    document.getElementById("log_in").innerHTML = '<svg role="status" class="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/> <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/> </svg> Loading...'
     let password = document.getElementById('psw').value;
     let username = document.getElementById('usrname').value;
 
@@ -251,6 +328,7 @@ function loginUser() {
 }
 
 function reqListener3() {
+  
     if (this.responseText === 'success') {
         window.location.href = '/dashboard';
         
@@ -262,5 +340,6 @@ function reqListener3() {
         let fail = document.getElementById('fail');
         fail.style.color = 'red';
         fail.innerHTML = 'There was an error when signing in';
+      document.getElementById("log_in").innerHTML = "Log In"
     }
 }
