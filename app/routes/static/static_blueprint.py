1446491bf98f9da50e0c938aa6268b24bdc397eb
NicholasWang <nicholas.x.wang@gmail.com>
2022-03-18 10:29:48 -0700

    fixed static files erorr

--- app/routes/static/static_blueprint.py
+++ app/routes/static/static_blueprint.py
@@ -5,11 +5,12 @@ static_blueprint = Blueprint('static_blueprint', __name__, url_prefix="/static",
                              static_folder="../static")
 
 
+# fixed: FileNotFoundError: [Errno 2] No such file or directory: '/Users/<>/IdeaProjects/ProjectNebulus/app/routes/app/static/images/logo.png'
 @static_blueprint.route("/<folder>/<file>")
 def static1(folder, file):
-    return send_file(f"app/static/{folder}/{file}")
+    return send_file(f"../static/{folder}/{file}")
 
 
 @static_blueprint.route("/<folder>/<folder2>/<file>")
 def static2(folder, folder2, file):
-    return send_file(f"app/static/{folder}/{folder2}/{file}")
+    return send_file(f"../static/{folder}/{folder2}/{file}")
