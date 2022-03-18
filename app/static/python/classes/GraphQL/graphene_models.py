7bf5f9169e6184e8e7701b899e19de7ece122fa0
CoderN-P <neel.parpia@gmail.com>
2022-03-18 07:19:52 -0700

    Fixed Assertion Error with GraphQL models

--- app/static/python/classes/GraphQL/graphene_models.py
+++ app/static/python/classes/GraphQL/graphene_models.py
@@ -1,19 +1,21 @@
 from graphene_mongo import MongoengineObjectType
 
-from app.static.python.classes import Announcement as AnnouncementModel
-from app.static.python.classes import Assignment as AssignmentModel
-from app.static.python.classes import Avatar as AvatarModel
-from app.static.python.classes import AvatarSize as AvatarSizeModel
-from app.static.python.classes import Course as CourseModel
-from app.static.python.classes import DocumentFile as DocumentModel
-from app.static.python.classes import Event as EventModel
-from app.static.python.classes import Folder as FolderModel
-from app.static.python.classes import Grades as GradesModel
-from app.static.python.classes import Schoology as SchoologyModel
-from app.static.python.classes import User as UserModel
+
+from ..Assignment import Assignment as AssignmentModel
+from ..Course import Course as CourseModel
+from ..Document import DocumentFile as DocumentModel
+from ..Events import Event as EventModel
+from ..Folder import Folder as FolderModel
+from ..Grades import Grades as GradesModel
+from ..Schoology import Schoology as SchoologyModel
+from ..User import User as UserModel
+from ..Avatar import Avatar as AvatarModel
+from ..Announcement import Announcement as AnnouncementModel
+from ..AvatarSize import AvatarSize as AvatarSizeModel
 
 
-# todo: fix AvatarSize, Avatar, and Announcement objects of "AssertionError: The attribute model in Announcement.Meta must be a valid Mongoengine Model. Received "<class 'module'>" instead."
+
+
 class User(MongoengineObjectType):
     class Meta:
         model = UserModel
