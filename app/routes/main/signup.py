45a63d21c714a1b97384c6b7353d3a4291e96d07
CoderN-P <neel.parpia@gmail.com>
2022-03-19 13:14:33 -0700

    fixed bug where signup redirected to signin

--- app/routes/main/signup.py
+++ app/routes/main/signup.py
@@ -1,11 +1,11 @@
-from flask import render_template, redirect
-from ...utils.logged_in import logged_in
+from flask import render_template, redirect, session
 from . import main_blueprint
 
 
 @main_blueprint.route("/signup", methods=["GET"])
-@logged_in
 def signup():
+    if session.get("logged_in"):
+        return redirect("/dashboard")
     return render_template(
         "main/signup.html", page="Nebulus - Sign Up", disablebar=True
     )
