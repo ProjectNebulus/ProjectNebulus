6f61af4f1854ad6f58858cf927d5b09df1f61299
NicholasWang <nicholas.x.wang@gmail.com>
2022-03-25 18:00:55 -0700

    added manual creating thing

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ from waitress import serve
 from flask import request
 from app.routes import init_app
 
+from app.static.python.mongodb import create
+
+create.createFolder({"name": "Week 1", "course": "1497078952970289156", "color": "red"})
+create.createAnnouncement(
+    {"title": "Week 1", "course": "1497078952970289156", "content": "Hello CLass!", "author": "Ms. Nagami"})
+create.createDocumentFile({"url": "https://bins.schoology.com/attachment/1940579309/source"
+                                  "/bcf2b7f6e0dc2df1df6a03d748d7c56d.docx"})
 app = init_app()
 print(app.url_map)
 app.secret_key = os.getenv("MONGOPASS")
