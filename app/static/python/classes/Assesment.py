fc63816d9b706ddd5b9dd33bcbf3091dfbf543ec
NicholasWang <nicholas.x.wang@gmail.com>
2022-03-27 08:11:11 -0700

    added assesment object + changed lms css

---
+++ app/static/python/classes/Assesment.py
@@ -0,0 +1,31 @@
+from mongoengine import *
+
+from .Snowflake import Snowflake
+
+
+class Assesment(Snowflake):
+    """
+    A subclass of the snowflake object, representing an assignment in a course.
+
+    Has a bidirectional relationship with the Course class. -> Course.assignments -> Assignment.course
+
+    This allows for easy access to the course that the assignment is in,
+    and also allows for easy access to the assignments in the course.
+
+    """
+
+    meta = {"collection": "Assesments"}
+    course = ReferenceField(
+        "Course", required=True, description="The course that this assignment is in."
+    )
+    due = DateTimeField(required=True, description="The due date of the assignment.")
+    title = StringField(required=True, description="The title of the assignment.")
+    points = IntField(
+        default=100, description="The number of points the assignment is worth."
+    )
+    description = StringField(
+        default="", null=True, description="The description of the assignment."
+    )
+    link = StringField(
+        default="", null=True, description="Link to the LMS"
+    )
