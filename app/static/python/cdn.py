6225a5e8708baf58023717e79ecf6b63cf78b2fd
NicholasWang <nicholas.x.wang@gmail.com>
2022-03-29 10:39:05 -0700

    finished cdn and added home

--- app/static/python/cdn.py
+++ app/static/python/cdn.py
@@ -7,14 +7,14 @@ client = session.client('s3',
                         endpoint_url='https://nebulus-cdn.sfo3.digitaloceanspaces.com',
                         # Find your endpoint in the control panel, under Settings. Prepend "https://".
                         region_name='sfo3',  # Use the region in your endpoint.
-                        aws_access_key_id='HW5ODS5FRC3ABZUBZMUI',
+                        aws_access_key_id='ZITBOFJ4QRQA4WBLMO33',
                         # Access key pair. You can create access key pairs using the control panel or API.
-                        aws_secret_access_key="PXVzHLe+Be77KjPzZsWYD4eUyP2HuoSYgrC4/XOnpc8",
+                        aws_secret_access_key="GnHgjOQ8ZorAnckiT5uRskFFwNqndEmhcWwtesr1LYM",
                         # Secret access key defined through an environment variable.
                         )
 # Step 3: Call the put_object command and specify the file to upload.
-client.upload_file('/Users/NicholasWang/IdeaProjects/ProjectNebulus/app/static/images/logo.png', 'nebulus-cdn',
-                   'logo.png')
+print(client.upload_file('/Users/NicholasWang/IdeaProjects/ProjectNebulus/app/static/images/logo.png', 'nebulus-cdn',
+                         'logo.png'))
 base = "https://nebulus-cdn.sfo3.digitaloceanspaces.com/"
 input = "nebulus-cdn/logo.png"
 print(base + input)
