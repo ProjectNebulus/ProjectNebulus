0a6f4bec237197f7e855f99829dfe89babb57175
NicholasWang <nicholas.x.wang@gmail.com>
2022-03-30 12:00:08 -0700

    fixed .. error

--- app/static/python/cdn.py
+++ app/static/python/cdn.py
@@ -4,13 +4,19 @@ from botocore.client import Config
 ACCESS_ID = 'VCRH4QOPCDEQR5PFNPQM'
 SECRET_KEY = 'Vx3pqPbLTGlSvNAhfxPFDto8CskcWOXOjvcW0ziwXys'
 
-# Initiate session
-session = session.Session()
-client = session.client('s3',
-                        region_name='sfo3',
-                        endpoint_url='https://sfo3.digitaloceanspaces.com',
-                        aws_access_key_id=ACCESS_ID,
-                        aws_secret_access_key=SECRET_KEY)
+def upload_file(file_name, path):
+    from boto3 import session
+    # Initiate session
+    session = session.Session()
+    client = session.client('s3',
+                            region_name='sfo3',
+                            endpoint_url='https://sfo3.digitaloceanspaces.com',
+                            aws_access_key_id=ACCESS_ID,
+                            aws_secret_access_key=SECRET_KEY)
 
-# Upload a file to your Space
-client.upload_file('cdn.py', 'nebulus-cdn', 'testing.py')
+    # Upload a file to your Space
+    client.upload_file(path, 'nebulus-cdn', file_name)
+
+
+upload_file("main.py", "../../../main.py")
+# upload_file("main.py", "......main.py")
