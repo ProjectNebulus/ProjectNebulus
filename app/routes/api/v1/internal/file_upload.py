973e50e4ec387cd632e694100a5f97ffaf0fe7bb
NicholasWang <nicholas.x.wang@gmail.com>
2022-03-31 06:15:55 -0700

    added mongodb to file upload

--- app/routes/api/v1/internal/file_upload.py
+++ app/routes/api/v1/internal/file_upload.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from .....static.python import cdn
+from .....static.python.mongodb import create
 
 UPLOAD_FOLDER = 'app/static/'
 UPLOAD_FOLDER_CDN = "../"
@@ -24,22 +25,28 @@ def allowed_file(filename):
 
 @internal.route('/upload_file', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return "No File Selected"
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect("/")
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            cdn.upload_file(filename, os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return ('', 204)
+    course = int(request.form.get("course"))
+    folder = int(request.form.get("folder"))
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return "no-file"
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('no-file')
+        return redirect("/")
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        cdn.upload_file(filename, os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if folder == 0:
+            create.createDocumentFile({"name": filename, "url": "https://cdn.nebulus.ml/" + filename, "course": course})
         else:
-            return "Bad File"
+            create.createDocumentFile(
+                {"name": filename, "url": "https://cdn.nebulus.ml/" + filename, "course": course, "folder": folder})
+        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return "success"
+    else:
+        return "invalid-file"
