4719d2119cc4bd5798070a30e62dc24715ab7511
CoderN-P <neel.parpia@gmail.com>
2022-04-10 11:35:31 -0700

    Added a limit to the amount of announcements and events on the dashboard.

--- app/routes/main/dashboard.py
+++ app/routes/main/dashboard.py
@@ -12,6 +12,10 @@ def dashboard():
     new_user = request.args.get("new_user", default="false", type=str)
     user_courses = read.get_user_courses(session.get("id"))
     sorted = read.unsorted_user_events(session["id"])
+    if len(sorted[0]) > 4:
+        sorted = sorted[0][-4:]
+    if len(sorted[1]) > 4:
+        sorted[1] = sorted[0][-4:]
     return render_template(
         "dashboard.html",
         password=session["password"],
