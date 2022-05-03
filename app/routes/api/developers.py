273b4fea17db4ea369f07d0e9fb551713d15742f
NicholasWang <nicholas.x.wang@gmail.com>
2022-05-02 20:46:25 -0700

    some changes for the public pages

--- app/routes/api/developers.py
+++ app/routes/api/developers.py
@@ -2,9 +2,11 @@ from flask import render_template, session
 
 from . import api_blueprint
 from ...static.python.mongodb import read
+from ..main.utils import logged_in
 
 
 @api_blueprint.route("/developers", methods=["GET"])
+@logged_in
 def api():
     return render_template(
         "developerportal.html",
