d9a5f983e88a03df6dbae0715a928cd597db0978
NicholasWang <nicholas.x.wang@gmail.com>
2022-05-03 06:39:52 -0700

    fixed bug

--- app/routes/api/v1/internal/create_user.py
+++ app/routes/api/v1/internal/create_user.py
@@ -38,7 +38,7 @@ def create_user():
 
     data["avatar"] = "https://localhost:8080/"+cats[int(data["avatar"].replace("cat", ""))]
     data["avatar"] = Avatar(avatar_url=data["avatar"], parent="User")
-    data["age"] = datetime.strptime(data["age"], '%m/%d/%y')
+    data["age"] = datetime.strptime(data["age"].strip(), '%m/%d/%y')
     validation = create.create_user(data)
     if validation[0] == "0":
         session["username"] = validation[1].username
