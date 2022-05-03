608e930103c44eab8780063392c87f29ab894665
CoderN-P <neel.parpia@gmail.com>
2022-05-02 18:51:09 -0700

    Small commit (mostly to test actions)

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,4 @@
 # Imports
-
 from flask_cors import CORS
 from flask_mail import Mail
 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 App entrypoint.
 """
 import os
+from flask import request
 
 from waitress import serve
 
@@ -13,4 +14,5 @@ regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
 
 if __name__ == "__main__":
     print("Started Running: http://localhost:8080")
-    serve(app=app, host="0.0.0.0", port=8080, threads=6)
+    app.run(host="localhost", port=80)
+
