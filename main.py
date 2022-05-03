b4ad822212b6099b0e0f398dc356c015e92f08e0
NicholasWang <nicholas.x.wang@gmail.com>
2022-05-02 18:57:05 -0700

    added cool bkg to signup

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
 
 if __name__ == "__main__":
     print("Started Running: http://localhost:8080")
-    app.run(host="localhost", port=80)
+    app.run(host="localhost", port=8080)
 
