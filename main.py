19250d00c38d733ebad14aa30795253e64372946
NicholasWang <nicholas.x.wang@gmail.com>
2022-05-05 12:20:09 -0700

    fixed final css bug

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
 if __name__ == "__main__":
     if platform.system().lower() == "linux" :
         port = 80
+        host = "0.0.0.0"
     else: #macos (darwin) or windows (windows)
         port = 8080
-    print(f"Started Running: http://localhost:{port}")
-    serve(app, host="0.0.0.0", port=port)
+        host = "localhost"
+    print(f"Started Running: http://{host}:{port}")
+    serve(app, host=host, port=port)
 
 
