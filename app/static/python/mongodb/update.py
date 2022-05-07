631031dcea51f119d70076f2ba6e82a72cd06659
CoderN-P <neel.parpia@gmail.com>
2022-05-07 10:53:56 -0700

    Fixed the problem with schoology connection changing passwords

--- app/static/python/mongodb/update.py
+++ app/static/python/mongodb/update.py
@@ -6,17 +6,17 @@ def schoologyLogin(_id: str, schoology: dict):
     user = find_user(pk=_id)
     if not user:
         raise KeyError("User not found")
+
     schoology = Schoology(**schoology)
     user.schoology.append(schoology)
-    user.save()
-
+    user.save(clean=False)
 
 def logout_from_schoology(_id: str):
     user = find_user(id=_id)
     if not user:
         raise KeyError("User not found")
     user.schoology = None
-    user.save()
+    user.save(clean=False)
     return "true"
 
 
