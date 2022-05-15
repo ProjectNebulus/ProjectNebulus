0ac3f05fd2b46351d993b882c04cea9547d0dcf3
CoderN-P <neel.parpia@gmail.com>
2022-05-15 10:19:57 -0700

    Kinda got the URL thing to work (DOES NOT WORK THAT WELL)

--- app/static/js/createObjects/lms.js
+++ app/static/js/createObjects/lms.js
@@ -1,16 +1,22 @@
-$(window).scroll(function(){
 
-    // get the bottom position
-    var bottom_position = $(document).height() - ($(window).scrollTop() + $(window).height());
-    var scroll_data = {
-            action: 'user_scroll',
-            container_id: container_id
-        };
-    $.ajax({
-             url: wdm_url,
-             data: scroll_data,
-             context: document.body
-           }).done(function(response) {
-                // do stuff here
-              });
-});
+function replaceURLs(message) {
+    console.log(message);
+    if(!message) return;
+
+    var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+    let result = message.replace(urlRegex, function (url) {
+        var hyperlink = url;
+        console.log(hyperlink);
+        if (!hyperlink.match('^https?:\/\/')) {
+          hyperlink = 'http://' + hyperlink;
+        }
+        return `<a class="
+                   underline 
+                   decoration-yellow-500 decoration-[0.25rem] 
+                   motion-safe:transition-all motion-safe:duration-200 
+                   hover:decoration-[0.5rem] focus:decoration-[0.5rem] hover:decoration-yellow-500/50 focus:decoration-yellow-500/50 
+               " href="${hyperlink}">${hyperlink}</a>`;
+  });
+    console.log(result);
+  return result;
+}
