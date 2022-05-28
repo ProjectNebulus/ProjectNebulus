dddf4fd9f7de9d8e72ad3e3d5a1564608fba0321
CoderN-P <neel.parpia@gmail.com>
2022-05-28 09:48:19 -0700

    Created a basic Chat object

---
+++ app/static/python/classes/Chat.py
@@ -0,0 +1,12 @@
+from mongoengine import *
+from .Snowflake import Snowflake
+
+class Chat(Snowflake):
+    members = ListField(ReferenceField('User'), required=True)
+    owner = ReferenceField('User', required=True)
+    created = DateTimeField(required=True)
+    title = StringField(required=False)
+
+    def clean(self):
+        if not self.title:
+            self.title = f"{self.owner}'s Chat"
