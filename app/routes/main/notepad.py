ae95ace6aacdfcf5f0c51066c8941c6488ac3520
NicholasWang <nicholas.x.wang@gmail.com>
2022-06-03 20:40:51 -0700

    added more fonts

--- app/routes/main/notepad.py
+++ app/routes/main/notepad.py
@@ -6,10 +6,24 @@ from ...static.python.mongodb import read
 
 @main_blueprint.route("/notepad", methods=["GET"])
 def notepad():
+    fonts = [
+        "Arial",
+        "Times New Roman",
+        "Comic Sans MS",
+        "Pacifico",
+        "Roboto",
+        "Lobster",
+        "Lato",
+        "Montserrat",
+        "Raleway",
+        "Roboto Condensed",
+        "Open Sans",
+    ]
     return render_template(
         "notepad.html",
         page="Nebulus - Notepad",
         user=session.get("username"),
         avatar="/static/images/nebulusCats" + session.get("avatar", "/v3.gif"),
         read=read,
+        fonts = fonts,
     )
