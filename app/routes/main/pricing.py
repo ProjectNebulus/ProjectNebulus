986f635974e40ba60375871ec3b07049678d74e4
NicholasWang <nicholas.x.wang@gmail.com>
2022-06-05 16:42:17 -0700

    fixed some bugs

--- app/routes/main/pricing.py
+++ app/routes/main/pricing.py
@@ -1,7 +1,10 @@
 from . import main_blueprint
-from flask import render_template
+from flask import render_template, session
 
 
 @main_blueprint.route("/pricing", methods=["GET"])
 def pricing():
-    return render_template("pricing.html", page="Pricing - Nebulus")
+    return render_template("pricing.html", page="Pricing - Nebulus",
+    user=session.get("username"),
+         email=session.get("email"),
+               avatar="/static/images/nebulusCats" + session.get("avatar", "/v3.gif"),)
