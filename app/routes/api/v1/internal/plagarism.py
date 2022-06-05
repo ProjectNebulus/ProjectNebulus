4feb100774b71fdbb3bfd0ddbcc2e08a212a4ded
NicholasWang <nicholas.x.wang@gmail.com>
2022-06-05 15:50:04 -0700

    more ui changes

--- app/routes/api/v1/internal/plagarism.py
+++ app/routes/api/v1/internal/plagarism.py
@@ -14,6 +14,10 @@ def plagarism():
     text2 = data["text2"]
     try:
         result = daplagarism(text1, text2)
+        if result[1] == True:
+            result[1] = "This is Plagarized by: "
+        else:
+            result[1] = "This isn't Plagarized. Your plagarism score is: "
     except:
         return "short"
     return f"{result[0]}•{result[1]}•{result[2]}"
