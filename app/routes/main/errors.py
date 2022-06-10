5e8cf856a0bcb3a1ffb33dc2c23d734f56357151
NicholasWang <nicholas.x.wang@gmail.com>
2022-06-10 11:09:28 -0700

    fixed small bugs

--- app/routes/main/errors.py
+++ app/routes/main/errors.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect
 
 from app.routes import main_blueprint
 
@@ -6,6 +6,10 @@ from app.routes import main_blueprint
 @main_blueprint.errorhandler(404)
 @main_blueprint.errorhandler(405)
 def error404(e):
+    path = request.path
+    print(path)
+    if len(path.strip("/")) == 2:
+        return redirect(f"/global/{path}")
     return render_template("errors/404.html", page="404 Not Found")
 
 
--- app/routes/main/index.py
+++ app/routes/main/index.py
@@ -67,6 +67,10 @@ def tos():
 @main_blueprint.app_errorhandler(404)
 @main_blueprint.app_errorhandler(400)
 def page_not_found(e):
+    path = request.path
+    print(path)
+    if len(path.strip("/")) == 2:
+        return redirect(f"/global/{path}")
     # note that we set the 404 status explicitly
     return render_template("errors/404.html"), 404
 
@@ -82,6 +86,7 @@ def sw():
     path = Path(__file__)
     print(path.parent.parent.parent)
     return send_file(str(path.parent.parent.parent)+'/static/js/sw.js')
+
 
 @main_blueprint.route("/global/<country>", methods=["GET"])
 def international(country):
@@ -113,10 +118,13 @@ def international(country):
         page = "Nebulus - 学習、オールインワン"
     if country == 'us':
         return redirect("/")
-    return render_template(
-        f"main/global/{country}.html",
-        page=page,
-        user=session.get("username"),
-        email=session.get("email"),
-        avatar="/static/images/nebulusCats" + session.get("avatar", "/v3.gif"),
-    )
+    try:
+        return render_template(
+            f"main/global/{country}.html",
+            page=page,
+            user=session.get("username"),
+            email=session.get("email"),
+            avatar="/static/images/nebulusCats" + session.get("avatar", "/v3.gif"),
+        )
+    except:
+        return "This country doesn't exist yet :( Contact norachai on Discord and request this country to be made."
