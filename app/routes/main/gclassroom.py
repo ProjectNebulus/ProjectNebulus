48ad4ae4c5fdf199725749764b73f6d4d184412d
NicholasWang <nicholas.x.wang@gmail.com>
2022-06-13 08:01:42 -0700

    fixed some bugs

--- app/routes/main/gclassroom.py
+++ app/routes/main/gclassroom.py
@@ -3,7 +3,7 @@ import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import requests
 from flask import render_template, session, request, redirect
-
+import os
 from . import main_blueprint
 from .utils import logged_in
 from ...static.python.gclassroomcom import *
@@ -15,12 +15,7 @@ from googleapiclient.discovery import build
 os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = "None"
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-# This variable specifies the name of a file that contains the OAuth 2.0
-# information for this application, including its client_id and client_secret.
 CLIENT_SECRETS_FILE = "app/static/python/credentials.json"
-
-# This OAuth 2.0 access scope allows for full read/write access to the
-# authenticated user's account and requires requests to use an SSL connection.
 SCOPES = [
     "https://www.googleapis.com/auth/classroom.courses.readonly",
     "https://www.googleapis.com/auth/classroom.rosters.readonly",
@@ -38,15 +33,6 @@ SCOPES = [
 ]
 API_SERVICE_NAME = "classroom"
 API_VERSION = "v1"
-
-
-# app = flask.Flask(__name__)
-# # Note: A secret key is included in the sample so that it works.
-# # If you use this code in your application, replace this with a truly secret
-# # key. See https://flask.palletsprojects.com/quickstart/#sessions.
-# app.secret_key = 'REPLACE ME - this value is here as a placeholder.'
-
-
 @main_blueprint.route("/gclassroom")
 @logged_in
 def gtest_api_request():
@@ -112,7 +98,7 @@ def authorize():
 
     # Store the state so the callback can verify the auth server response.
     session["state"] = state
-
+    session["redirect_url_g"] = authorization_url
     return redirect(authorization_url)
 
 
@@ -130,9 +116,12 @@ def oauth2callback():
     flow.redirect_uri = flask.url_for("main_blueprint.oauth2callback", _external=True)
     print(flow.redirect_uri)
     # Use the authorization server's response to fetch the OAuth 2.0 tokens.
-    authorization_response = flask.request.url
+    #authorization_response = flask.request.url
+    authorization_response = session["redirect_url_g"]+"&code="+request.args.get("code")
+    if "http://beta.nebulus.ml" in authorization_response:
+        authorization_response = authorization_response.replace("http", "https")
+    #flow.fetch_token(authorization_response=authorization_response)
     flow.fetch_token(authorization_response=authorization_response)
-
     # Store credentials in the session.
     # ACTION ITEM: In a production app, you likely want to save these
     #              credentials in a persistent database instead.
@@ -147,10 +136,7 @@ def oauth2callback():
 @logged_in
 def revoke():
     if "credentials" not in flask.session:
-        return (
-            'You need to <a href="/authorize">authorize</a> before '
-            + "testing the code to revoke credentials."
-        )
+        return redirect("/settings")
 
     credentials = google.oauth2.credentials.Credentials(**flask.session["credentials"])
 
@@ -172,7 +158,7 @@ def revoke():
 def clear_credentials():
     if "credentials" in flask.session:
         del flask.session["credentials"]
-    return "Credentials have been cleared.<br><br>" + print_index_table()
+    return redirect("/settings")
 
 
 def credentials_to_dict(credentials):
@@ -184,39 +170,3 @@ def credentials_to_dict(credentials):
         "client_secret": credentials.client_secret,
         "scopes": credentials.scopes,
     }
-
-
-def print_index_table():
-    return (
-        "<table>"
-        + '<tr><td><a href="/test">Test an API request</a></td>'
-        + "<td>Submit an API request and see a formatted JSON response. "
-        + "    Go through the authorization flow if there are no stored "
-        + "    credentials for the user.</td></tr>"
-        + '<tr><td><a href="/authorize">Test the auth flow directly</a></td>'
-        + "<td>Go directly to the authorization flow. If there are stored "
-        + "    credentials, you still might not be prompted to reauthorize "
-        + "    the application.</td></tr>"
-        + '<tr><td><a href="/revoke">Revoke current credentials</a></td>'
-        + "<td>Revoke the access token associated with the current user "
-        + "    session. After revoking credentials, if you go to the test "
-        + "    page, you should see an <code>invalid_grant</code> error."
-        + "</td></tr>"
-        + '<tr><td><a href="/clear">Clear Flask session credentials</a></td>'
-        + "<td>Clear the access token currently stored in the user session. "
-        + '    After clearing the token, if you <a href="/test">test the '
-        + "    API request</a> again, you should go back to the auth flow."
-        + "</td></tr></table>"
-    )
-
-
-#
-# if __name__ == '__main__':
-#     # When running locally, disable OAuthlib's HTTPs verification.
-#     # ACTION ITEM for developers:
-#     #     When running in production *do not* leave this option enabled.
-#     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-#
-#     # Specify a hostname and port that are set as a valid redirect URI
-#     # for your API project in the Google API Console.
-#     app.run('localhost', 8080, debug=True)
