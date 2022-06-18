8e0270b60857ee2ce8827664fd2f4e0b0c5bde9a
Python Coder <fdsnail2@gmail.com>
2022-06-17 19:45:47 -0800

    fixed some css

--- app/routes/api/v1/internal/search.py
+++ app/routes/api/v1/internal/search.py
@@ -1,5 +1,6 @@
 # TODO
 from flask import request, session
+
 from . import internal
 from .....static.python.mongodb import read
 
@@ -7,4 +8,4 @@ from .....static.python.mongodb import read
 @internal.route("/search-object", methods=["POST"])
 def search_object():
     data = request.get_json()
-    results = read.search(data["keyword"], session["id"])
+    results = read.search(data["keyword"], session["username"])
