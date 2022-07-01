4ec6cec6dd64f33d8bde86f3c6b66f0377f94229
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-06-30 21:26:51 -0700

    FINALLY FIXED EMAIL VERIFY

--- app/routes/api/v1/internal/check_verification_code.py
+++ app/routes/api/v1/internal/check_verification_code.py
@@ -1,9 +1,10 @@
-from flask import session, request
-
+from flask import session, request, jsonify
+import json
 from . import internal
 
 
 @internal.route("/check-verification-code", methods=["POST"])
 def check_email_code():
-    var = request.json["value"]
+    #var = jsonify(next(request.form.items())[0])["value"]
+    var = json.loads(next(request.form.items())[0])["value"]
     return str(var == str(session["verificationCode"])).lower()
