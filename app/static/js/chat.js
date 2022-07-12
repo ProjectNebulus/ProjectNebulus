69d378c7d2b5c478420a2fa30822ed1641c9e5d2
CoderN-P <neel.parpia@gmail.com>
2022-07-12 06:24:20 +0530

    Fixes #224

--- app/static/js/chat.js
+++ app/static/js/chat.js
@@ -413,7 +413,7 @@ function load(data) {
             }
             s += `
         <div class="space-y-1 font-medium dark:text-white">
-            <div class="dark:text-gray-300" style="font-size:20px">${other['username']}</div>
+            <div class="dark:text-gray-300" style="font-size:20px" id="${chat['id']}_title" >${other['username']}</div>
             <div class="text-sm text-gray-500 dark:text-gray-400" style="font-size:13px;">${status_emoji} ${status_text}</div>
         </div>
     </div>
@@ -430,7 +430,7 @@ function load(data) {
             }
 
             s += `<div class="space-y-1 font-medium dark:text-white">
-    <div style="font-size:20px;" class="dark:text-gray-300">${chat['title']}</div>
+    <div style="font-size:20px;" id="${chat['id']}_title" class="dark:text-gray-300">${chat['title']}</div>
     <div style="font-size:13px;" class="text-sm text-gray-500 dark:text-gray-400">${chat['members'].length} Members</div>
 </div>
 </div>`;
@@ -571,6 +571,7 @@ function getChat(chatID) {
             let chatMembers = ``;
             let el = document.getElementById(`sidechat_${chat['_id']}`);
             let userChats = document.getElementById('user-chats');
+            let userID = document.getElementById('user-data').textContent;
             userChats = [...userChats.children];
             userChats.map(function (elem) {
                 elem.classList.remove('dark:bg-gray-600');
@@ -619,6 +620,72 @@ function getChat(chatID) {
                 chat_el.insertAdjacentHTML('beforeend', chatContent);
                 chatContent = ``;
             });
+            let tx;
+            let ms = document.getElementById(`${chat['id']}_title`).innerText;
+            if (chat['members'].length == 2){
+                tx = `
+            <div class = "flex items-center py-2 px-3 bg-gray-50/50 dark:bg-gray-700/50 backdrop-blur-sm dark:text-white text-black py-3 noselect " >
+                <i class="material-icons text-gray-400"> alternate_email </i>
+            <span class="mx-4">${ms}</span>
+            <span class="inline-block w-3 h-3 bg-green-400 rounded-full"></span>
+        </div>
+        `;
+                ms = '@'+ms;
+            } else {
+                tx = `
+            <div
+            class = "flex items-center py-2 px-3 bg-gray-50/50 dark:bg-gray-700/50 backdrop-blur-sm dark:text-white text-black py-3 noselect " >
+            <span class="mx-4">${ms}</span>
+        </div>
+        `;
+            }
+
+
+
+
+
+            chat_el.insertAdjacentHTML('beforebegin', tx);
+
+            chat_el.insertAdjacentHTML('afterend', `<form id="msg" style="width:100%;" onsubmit="sendMessage(); return false;">
+                <label for="chat" class="sr-only">Your message</label>
+                <div class="flex items-center py-2 px-3 bg-gray-50 rounded-lg dark:bg-gray-700 m-6 ">
+                    <button type="button"
+                            class="hidden md:block inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                  d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
+                                  clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                    <button data-dropdown-toggle="emojiPicker" data-dropdown-placement="top" type="button"
+                            class="hidden md:block p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                  d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.415-1.414 3 3 0 01-4.242 0 1 1 0 00-1.415 1.414 5 5 0 007.072 0z"
+                                  clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                    <div id="emojiPicker"
+                         class="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700">
+                    </div>
+                    <div class="block mx-4 p-2.5 w-full">
+                        <div id="preview" class="roboto text-black dark:text-white"></div>
+                        <br><br>
+                        <textarea id="msg_content" rows="1"
+                                  class="block p-2.5 w-full text-sm text-gray-900 dark:border-gray-700 border-white dark:bg-gray-700 bg-white dark:text-white"
+                                  placeholder="Message ${ms}" required></textarea>
+                    </div>
+                    <button type="submit"
+                            class="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
+                        <svg class="w-6 h-6 rotate-90" fill="currentColor" viewBox="0 0 20 20"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                        </svg>
+                    </button>
+                </div>
+            </form>`);
 
             chat['members'].forEach(function (other) {
                 chatMembers += `<div 
