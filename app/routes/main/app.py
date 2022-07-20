ea0d01567e6ed2deb059c4585337416dde40f8c3
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-07-19 17:02:41 -0700

    added /courses

--- app/routes/main/app.py
+++ app/routes/main/app.py
@@ -149,3 +149,90 @@ def app():
         Event=Event,
         pastschoologycourses=scCourses,
     )
+
+
+@main_blueprint.route("/courses", methods=["GET"])
+@logged_in
+def courses():
+    user_acc = read.find_user(id=session["id"])
+    user_courses = read.get_user_courses(session["id"])
+    events = read.sort_user_events(session["id"])
+
+    try:
+        gcourses = getGclassroomcourses()
+    except:
+        gcourses = []
+
+    canvascourses = []
+    try:
+        from canvasapi import Canvas
+
+        API_URL = session["canvas_link"]
+        API_KEY = session["canvas_key"]
+        canvas = Canvas(API_URL, API_KEY)
+        account = canvas.get_user(user="self")
+        courses = account.get_courses()
+        for course in courses:
+            original_name = ""
+            try:
+                original_name = course.original_name
+            except:
+                original_name = course.name
+            canvascourses.append(
+                [course.name, f"{API_URL}/course/{course.id}", original_name]
+            )
+    except Exception as e:
+        canvascourses = []
+
+    scCourses = []
+    import schoolopy
+
+    try:
+        schoology = read.getSchoology(username=session["username"])
+        if len(schoology) > 0:
+            schoology = schoology[0]
+            key = schoology.apikey or session.get("request_token") or "eb0cdb39ce8fb1f54e691bf5606564ab0605d4def"
+            secret = schoology.apisecret or session.get("request_token_secret") or "59ccaaeb93ba02570b1281e1b0a90e18"
+
+            auth = schoolopy.Auth(
+                key,
+                secret,
+                domain=schoology.schoologyDomain,
+                three_legged=True,
+                request_token=schoology.Schoology_request_token,
+                request_token_secret=schoology.Schoology_request_secret,
+                access_token=schoology.Schoology_access_token,
+                access_token_secret=schoology.Schoology_access_secret,
+            )
+            auth.authorize()
+            sc = schoolopy.Schoology(auth)
+            sc.limit = "100&include_past=1"
+            scCourses = list(sc.get_user_sections(user_id=sc.get_me().id))
+            for i in range(0, len(scCourses)):
+                scCourses[i] = dict(scCourses[i])
+                scCourses[i]["link"] = (
+                        schoology.schoologyDomain
+                        + "course/"
+                        + scCourses[i]["id"]
+                        + "/materials"
+                )
+            scSchool = sc.get_school(scCourses[0]["school_id"])
+            scCourses.append(scSchool)
+    except Exception as e:
+        print(session)
+        print(e)
+        scCourses = []
+
+    return render_template(
+        "courses.html",
+        user=session["username"],
+        email=session.get("email"),
+        avatar=session.get("avatar", "/static/images/nebulusCats/v3.gif"),
+        user_acc=user_acc,
+        user_courses=list(user_courses),
+        page="Nebulus - Courses",
+        gcourses=gcourses,
+        canvascourses=canvascourses,
+        schoologycourses=scCourses,
+        pastschoologycourses=scCourses,
+    )
