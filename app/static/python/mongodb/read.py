1c96b531fb61febd8e46c2f26aa6236b1d8728c4
nicholasxwang <80929436+nicholasxwang@users.noreply.github.com>
2022-08-15 11:13:42 -0700

    Update read.py

--- app/static/python/mongodb/read.py
+++ app/static/python/mongodb/read.py
@@ -11,7 +11,7 @@ from ..classes import *
 
 regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
 
-with open("app/static/python/dictionary.json") as f:
+with open("/app/static/python/dictionary.json") as f:
     dictionary = json.loads(f.read())
 
 
