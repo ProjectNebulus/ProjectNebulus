f1e67f8bf429232edd7dcc8dc6f071c0ba7a1874
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-08-21 11:54:35 -0700

    made scrolling more smooth

--- app/static/js/chat.js
+++ app/static/js/chat.js
@@ -157,7 +157,7 @@ function makeCall(getFirst = true) {
         url: '/api/v1/internal/fetch-chats',
         type: 'POST',
         beforeSend: function () {
-            $('#user-chats').hide();
+            // $('#user-chats').hide();
             $('#chat-list-loading').show();
         },
         contentType: 'application/json',
@@ -368,9 +368,9 @@ $(document).ready(function () {
                 contentType: 'application/json',
                 beforeSend: function () {
 
-            document.getElementById('chat').classList.add('hidden');
-            $('#chat-loading').show();
-        },
+                    //document.getElementById('chat').classList.add('hidden');
+                    $('#chat-loading2').show();
+                },
                 data: JSON.stringify({
                     chatID: chatID,
                     current_index: chat_index
@@ -452,7 +452,7 @@ $(document).ready(function () {
                 chatContent = ``;
             });
 
-                 $('#chat-loading').hide();
+                $('#chat-loading2').hide();
                 let preview = document.getElementById('preview_border');
                 document.getElementById('chat').style.height = preview.style.display === "none" ? "80%" : "64%";
                 document.getElementById('chat').classList.remove('hidden');
@@ -743,7 +743,7 @@ function getChat(chatID) {
                 show_preview = true;
             }
 
-            document.getElementById('chat').classList.add('hidden');
+            //document.getElementById('chat').classList.add('hidden');
             $('#chat-members').hide();
             $('#chat-loading').show();
             $('#chat-member-loading').show();
@@ -893,6 +893,102 @@ function getChat(chatID) {
             <span class="mx-4">${ms}</span>
             <span class="inline-block w-3 h-3 ${other_color} rounded-full"></span>
         </div>
+        <div id="chat-loading2"
+                     style="height:calc(100% - 80px);
+                            overflow-y:hidden; display:none;">
+                    <div role="status"
+                         class="sticky overflow-y-scroll-auto p-4 max-w-lg rounded shadow md:p-6  animate-pulse"
+                         style="overflow-y:scroll;">
+                        <div class="w-full flex items-top mt-4 space-x-3">
+                            <svg class="w-14 h-14 text-gray-200 dark:text-gray-700"
+                                 aria-hidden="true"
+                                 fill="currentColor"
+                                 viewBox="0 0 20 20"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                                      clip-rule="evenodd">
+                                </path>
+                            </svg>
+                            <div class="mt-2 w-full">
+                                <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2">
+                                </div>
+                                <div class="w-48 mb-1 h-2 bg-gray-200 rounded-full dark:bg-gray-700">
+                                </div>
+                                <div class="w-full mb-2 h-2 bg-gray-200 rounded-full dark:bg-gray-700">
+                                </div>
+                                <div class="flex justify-center items-center mb-4 h-48 bg-gray-300 rounded dark:bg-gray-700">
+                                    <svg class="w-12 h-12 text-gray-200 dark:text-gray-600"
+                                         xmlns="http://www.w3.org/2000/svg"
+                                         aria-hidden="true"
+                                         fill="currentColor"
+                                         viewBox="0 0 640 512">
+                                        <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z">
+                                        </path>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="w-full flex items-center mt-4 space-x-3">
+                            <div class="w-full flex items-center mt-4 space-x-3">
+                                <svg class="w-14 h-14 text-gray-200 dark:text-gray-700"
+                                     aria-hidden="true"
+                                     fill="currentColor"
+                                     viewBox="0 0 20 20"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                          d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                                          clip-rule="evenodd">
+                                    </path>
+                                </svg>
+                                <div class="w-full">
+                                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2">
+                                    </div>
+                                    <div class="w-48 mt-1 h-2 bg-gray-200 rounded-full dark:bg-gray-700">
+                                    </div>
+                                    <div class="mt-1 w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-1 ">
+                                    </div>
+                                    <div class="w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="w-full flex items-top mt-4 space-x-3">
+                            <div class="w-full flex items-top mt-4 space-x-3">
+                                <svg class="w-14 h-14 text-gray-200 dark:text-gray-700"
+                                     aria-hidden="true"
+                                     fill="currentColor"
+                                     viewBox="0 0 20 20"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                          d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                                          clip-rule="evenodd">
+                                    </path>
+                                </svg>
+                                <div class="w-full">
+                                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2">
+                                    </div>
+                                    <div class="w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-1 ">
+                                    </div>
+                                    <div class="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
+                                    </div>
+                                    <div class="flex justify-center items-center mb-4 h-48 bg-gray-300 rounded dark:bg-gray-700">
+                                        <svg class="w-12 h-12 text-gray-200 dark:text-gray-600"
+                                             xmlns="http://www.w3.org/2000/svg"
+                                             aria-hidden="true"
+                                             fill="currentColor"
+                                             viewBox="0 0 640 512">
+                                            <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z">
+                                            </path>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <span class="sr-only">Loading...</span>
+                    </div>
+                    <span class="sr-only">Loading...</span>
+                </div>
         `;
                 ms = '@' + ms;
             } else {
