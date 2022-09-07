dfd5115a952f8cf9c18a476b8fad6e29db693783
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-09-07 06:17:16 -0700

    merged the two bars

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ if __name__ == "__main__":
         debug = False
         port = 80
         host = "0.0.0.0"
-        protocol = "s"
+        protocol = "https"
     else:  # macos (darwin) or windows (windows)
         port = 8080
         host = "localhost"
-        protocol = ""
+        protocol = "http"
 
     print(str(app.url_map).replace("Map([", " ", 1).replace("])", "\n"), sep="\n")
-    print(f"Started Running: http{protocol}://{host}:{port}")
+    print(f"Started Running: {protocol}://{host}:{port}")
     socketio.run(app, host=host, port=port, debug=debug)
